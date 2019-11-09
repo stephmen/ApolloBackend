ece4f225347b981e776273d40bdc302434b28341
stephmen <stephmen@gmail.com>
2019-11-09 16:29:05 -0500

    db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
 const { Prisma } =require('prisma-binding');
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'http://localhost:4466',
+  endpoint: '10.128.1.246:4466',
   secret: process.env.PRISMA_SECRET,
   debug: true,
 });
