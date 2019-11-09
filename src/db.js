eaeb37e7660bddb9c24a53ac54c2d1995718ade7
stephmen <stephmen@gmail.com>
2019-11-09 17:23:01 -0500

    Changed IP back to 172.30.47.189

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
 const { Prisma } =require('prisma-binding');
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: '10.128.1.246:4466',
+  endpoint: 'http://172.30.47.189:4466',
   secret: process.env.PRISMA_SECRET,
   debug: true,
 });
