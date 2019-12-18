51409686d65612e8a01431b61895dbe8ab13f99d
stephmen <stephmen@gmail.com>
2019-12-18 17:22:28 -0500

    changing IP in db.js

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
 const { Prisma } =require('prisma-binding');
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: process.env.PRISMA_ENDPOINT,
+  endpoint: '172.30.205.22:4466',
   secret: process.env.PRISMA_SECRET,
   debug: true,
 });
