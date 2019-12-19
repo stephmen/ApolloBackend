0977b7aa5ee1ae81afc6c3551c44a56196d1107b
stephmen <stephmen@gmail.com>
2019-12-18 20:59:40 -0500

    db.js IP address

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
 const { Prisma } =require('prisma-binding');
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: '172.30.205.22:4466',
+  endpoint: 'http://172.30.205.22:4466',
   secret: process.env.PRISMA_SECRET,
   debug: true,
 });
