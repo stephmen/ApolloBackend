3bb3df9519f3bc322a306e6c6cb0ba32e4541455
stephmen <stephmen@gmail.com>
2020-02-22 09:51:11 -0500

    Changed IP Address

--- src/db.js
+++ src/db.js
@@ -3,6 +3,7 @@ const { Prisma } = require('prisma-binding');
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   //endpoint: "http://172.30.113.53:4466",
+  endpoint: "172.30.28.167:4466",
   endpoint: "http://localhost:4466",
   secret: process.env.PRISMA_SECRET,
   debug: false,
