609867be2f29efee685d96cb28097ee97e352cad
stephmen <stephmen@gmail.com>
2020-02-22 18:29:36 -0500

    Changed db.js to work with process.env

--- src/db.js
+++ src/db.js
@@ -3,8 +3,9 @@ const { Prisma } = require('prisma-binding');
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   //endpoint: "http://172.30.113.53:4466",
-  endpoint: "172.30.28.167:4466",
-  endpoint: "http://localhost:4466",
+  //endpoint: "172.30.28.167:4466",
+  endpoint: process.env.PRISMA_ENDPOINT,
+  //endpoint: "http://localhost:4466",
   secret: process.env.PRISMA_SECRET,
   debug: false,
 });
