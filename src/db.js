f3de263658ee5eedff7e820c2ffff4ca911d8c15
stephmen <stephmen@gmail.com>
2020-03-02 17:25:51 -0500

    IP Errror

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   //endpoint: "http://172.30.113.53:4466",
   //endpoint: "http://172.30.28.167:4466",  
-  endpoint: "172.30.80.159:4466",
+  endpoint: "http://172.30.80.159:4466",
   //endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET,
   debug: false,
