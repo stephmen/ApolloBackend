7363f27775f18207bd468ea194c7a40610509e79
stephmen <stephmen@gmail.com>
2020-05-12 13:51:44 -0400

    working with process.env.WHITELIST

--- src/index.js
+++ src/index.js
@@ -81,6 +81,6 @@ server.applyMiddleware({
 
 app.listen({ port: 4444 }, () => {
   console.log(`🚀 Server ready at http://localhost:4444${server.graphqlPath}`)
-  console.log(process.env.$WHITELIST)
+  console.log(process.env.WHITELIST)
 } 
 );
