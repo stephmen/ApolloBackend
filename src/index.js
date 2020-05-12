83c78105cb29e4ac078dd736349dd3aac18daa00
stephmen <stephmen@gmail.com>
2020-05-12 15:25:49 -0400

    FIxing CORS for good

--- src/index.js
+++ src/index.js
@@ -26,12 +26,10 @@ const server = new ApolloServer({
 
 const app = express();
 
-var whitelist = ["http://prisma-development.apps-crc.testing", "prisma-development.apps-crc.testing", "http://172.30.241.100:4466", "172.30.52.150", "apollo-backend-development.apps-crc.testing", "http://apollo-backend-development.apps-crc.testing", "172.30.90.125", "frontend-development.apps-crc.testing", "http://frontend-development.apps-crc.testing", "http://checkout.stripe.com/", "http://192.168.64.2:4466"]
-
+const whitelist = process.env.WHITELIST.split(', ')
 
 var corsOptions = {
   origin: function (origin, callback) {
-    //if (process.env.$WHITELIST.indexOf(origin) !== -1) {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
@@ -40,15 +38,6 @@ var corsOptions = {
   },
   credentials: true // <-- REQUIRED backend setting
 }
-
-
-
-
-// var corsOptions = {
-//   origin: process.env.FRONTEND_URL, 
-//   credentials: true // <-- REQUIRED backend setting
-// };
-// //console.log(process.env.APP_SECRET)
 
 app.use(cors(corsOptions));
 app.use(cookieParser());
@@ -81,7 +70,5 @@ server.applyMiddleware({
 
 app.listen({ port: 4444 }, () => {
   console.log(`🚀 Server ready at http://localhost:4444${server.graphqlPath}`)
-  let whitelist2 = process.env.WHITELIST.split(', ')
-  console.log(whitelist2)
 } 
 );
